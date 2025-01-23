cc0b6fe1171f59f0faab1922cc3bed61c04a0b46
stanleymw <107821509+stanleymw@users.noreply.github.com>
2025-01-22 22:35:21 -0800

    gravity and hitbox

--- src/game.zig
+++ src/game.zig
@@ -7,7 +7,7 @@ const CAMERA_SPEED = 8;
 
 var plr = player.Player.init(
     .{ .x = 0, .y = 0, .z = 0 },
-    .{ .x = 0, .y = 0, .z = 0 },
+    .{ .x = 0, .y = -8, .z = 0 },
     .{
         .position = rl.Vector3.init(0, 16.0, 16.0),
         .target = rl.Vector3.init(0, 0, 0),
@@ -34,7 +34,7 @@ pub fn update(dt: f32) void {
         dx += 8;
     }
 
-    plr.velocity = rl.Vector3{ .x = dx, .y = 0, .z = dz };
+    plr.velocity = rl.Vector3{ .x = dx, .y = plr.velocity.y, .z = dz };
 
     // update player and camera
     plr.update(dt);
--- src/player.zig
+++ src/player.zig
@@ -4,6 +4,7 @@ pub const Player = struct {
     velocity: rl.Vector3,
     gravity: rl.Vector3,
     camera: rl.Camera3D,
+    hitbox: rl.Vector3 = rl.Vector3{ .x = 1, .y = 2, .z = 1 },
 
     pub fn init(vel: rl.Vector3, grav: rl.Vector3, cam: rl.Camera3D) Player {
         return Player{
@@ -13,8 +14,15 @@ pub const Player = struct {
         };
     }
 
+    pub fn onGround(self: *Player) bool {
+        return self.camera.position.y - self.hitbox.y <= 0;
+    }
+
     pub fn update(self: *Player, dt: f32) void {
         self.velocity = self.velocity.add(self.gravity.scale(dt));
+        if (onGround(self)) {
+            self.velocity.y = 0;
+        }
 
         const velocity_scaled = self.velocity.scale(dt);
 
